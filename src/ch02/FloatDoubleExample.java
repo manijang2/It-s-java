d2d317cf1b87d16cf3920b300bd0258cec841122
seonman.kim <seonman.kim@kakao.com>
2020-12-09 14:14:27 +0900

    2020-12-08 중간 공부 내용 추가

---
+++ src/ch02/FloatDoubleExample.java
@@ -0,0 +1,18 @@
+package ch02;
+
+public class FloatDoubleExample {
+    public static void main(String[] args) {
+        double var1 = 3.14;
+        //float var2 = 3.14;
+        float var3 = 3.14F;
+
+        double var4 = 0.1234567890123456789;
+        float var5 = 0.1234567890123456789F;
+
+        System.out.println(var1);
+        //System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+    }
+}
